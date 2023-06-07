abbcc11bee2823067a3ae21c0acf875fc00f43a4
Marcos S. Oliveira Silva <92735173+sosmarcos@users.noreply.github.com>
2023-06-06 21:17:12 -0300

    Delete Update
    Adicionado o pequeno menu de edição de valores, e a função deletar

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ var total = 0
 var registro = []
 var porcento3On = false
 var porcento5On = false
+var menuEspecificOn = false
+
+class ValorUnitario {
+    constructor(valor_especifico, texto) {
+        this.valor = valor_especifico
+        this.texto = texto
+        } 
+    }
 
 function soma() {
     var entrada = window.document.getElementById('entrada')
@@ -9,11 +17,19 @@ function soma() {
     var valor = parseFloat(entrada.value)
 
     total += valor
-    registro.push(valor.toFixed(2))
+    registro.push(new ValorUnitario(valor, `
+        <label id='linha${registro.length}' class='linhaRetorno' onclick='funçoesDoValor(${registro.length})'>
+            R&#36; ${valor.toFixed(2)}
+        </label>
+        <div id='funçoes${registro.length}' class="menuEspecifico">
+            <input type="button" class="funçoes" value="X" onclick="multiplicanção()">
+            <input type="button" class="funçoes" value="D" onclick="deletar(${registro.length})">
+        </div><br>`))
+    console.log(registro)
     
     retorno.innerText = ''
     for (let index in registro) {
-        retorno.innerHTML += `R&#36; ${registro[index]}<br>`
+        retorno.innerHTML += registro[index].texto
     }
     retorno.innerHTML += `Total: R&#36 ${total.toFixed(2)}<br>`
     entrada.value = ''
@@ -30,7 +46,7 @@ function porcento5() {
 
         retorno.innerText = ''
         for (let index in registro) {
-            retorno.innerHTML += `R&#36; ${registro[index]}<br>`
+            retorno.innerHTML += registro[index].texto
         }
         retorno.innerHTML += `R&#36; -${(total*5/100).toFixed(2)}<br>`
         retorno.innerHTML += `Total: R&#36 ${total5.toFixed(2)}<br>`
@@ -39,7 +55,7 @@ function porcento5() {
 
         retorno.innerText = ''
         for (let index in registro) {
-            retorno.innerHTML += `R&#36; ${registro[index]}<br>`
+            retorno.innerHTML += registro[index].texto
         }
         retorno.innerHTML += `Total: R&#36 ${total.toFixed(2)}<br>`
     }
@@ -54,7 +70,7 @@ function porcento3() {
 
         retorno.innerText = ''
         for (let index in registro) {
-            retorno.innerHTML += `R&#36; ${registro[index]}<br>`
+            retorno.innerHTML += registro[index].texto
         }
         retorno.innerHTML += `R&#36; -${(total*3/100).toFixed(2)}<br>`
         retorno.innerHTML += `Total: R&#36 ${total3.toFixed(2)}<br>`
@@ -63,14 +79,51 @@ function porcento3() {
 
         retorno.innerText = ''
         for (let index in registro) {
-            retorno.innerHTML += `R&#36; ${registro[index]}<br>`
+            retorno.innerHTML += registro[index].texto
         }
         retorno.innerHTML += `Total: R&#36 ${total.toFixed(2)}<br>`
     }
 }
 
-function multiplicação() {
+function funçoesDoValor(index) {
+    var multiplicando = registro[index]
+    var valorEmLinha = window.document.getElementById(`linha${index}`)
+    var menuEspecifico = window.document.getElementById(`funçoes${index}`)
 
+    for (let index in registro) {
+        let menu = window.document.getElementById(`funçoes${index}`)
+        if (menu.style.display == 'inline-block') {
+            if (menu != menuEspecifico) {
+                menu.style.display = 'none'
+                menuEspecificOn = false
+            }
+        }
+    }
+    if (!menuEspecificOn) {
+        menuEspecificOn = true
+        
+        menuEspecifico.style.display = 'inline-block'
+    } else {
+        menuEspecificOn = false
+        menuEspecifico.style.display = 'none'
+    }
+    
+}
+
+function multiplicanção() {
+    null
+}
+
+function deletar(index) {
+    total -= registro[index].valor
+
+    delete registro[index]
+    retorno.innerText = ''
+    for (let index in registro) {
+        retorno.innerHTML += registro[index].texto
+    }
+    retorno.innerHTML += `Total: R&#36 ${total.toFixed(2)}<br>`
+    menuEspecificOn = false
 }
 
 function menuExpand() {
